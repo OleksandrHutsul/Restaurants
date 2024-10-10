bc161f64a71c50f6a2999c1fe4cb45c714bf012d
Саша Гуцул <sasha11hutsul@gmail.com>
2024-10-10 18:57:14 +0300

    added db

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,15 +1,21 @@
+using Restaurants.Infrastructure.Extensions;
+using Restaurants.Infrastructure.Seeders;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+
+await seeder.Seed();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
